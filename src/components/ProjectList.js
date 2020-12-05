e90cb1603e306944ce2d28d0abd02a266b7c696c
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2020-12-05 17:47:57 -0500

    Change header size

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import Portfolio from './Portfolio';
 import { projects } from '../data';
 import Contact from './Contact';
+import { THEME } from '../components/GlobalStyle/Theme';
 
 
 const items = Object.values(projects);
@@ -28,7 +29,9 @@ const Wrapper = styled.div`
 `
 
 const Header = styled.h1`
-    
+    font-size: 28px;
+    border-left: solid 5px ${THEME.secondary};
+    padding-left: 5px;
 `
 
 export default ProjectList;
